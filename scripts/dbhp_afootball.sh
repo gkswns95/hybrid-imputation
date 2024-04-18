39e87c08fda41c2f3e5f07bfe68fb37a79c686f8
choi <95gkswns@naver.com>
2024-04-18 16:19:22 +0900

    📝 Modify name of aruments

--- scripts/dbhp_afootball.sh
+++ scripts/dbhp_afootball.sh
@@ -1,32 +1,33 @@
+CUDA_VISIBLE_DEVICES=0 \
 python train.py \
---trial 3012 \
---dataset football \
+--trial 300 \
+--dataset afootball \
 --model dbhp \
---target_type imputation \
 --missing_pattern playerwise \
---n_players 6 \
+--missing_rate 0.5 \
+--normalize \
+--team_size 6 \
 --n_features 6 \
---train_nfl \
---valid_nfl \
---pe_z_dim 64 \
---pi_z_dim 32 \
---rnn_dim 512 \
---hybrid_rnn_dim 512 \
---n_layers 2 \
---n_heads 4 \
---dropout 0.0 \
---physics_loss \
---cartesian_accel \
---fpe \
---fpi \
---dynamic_hybrid \
---bidirectional \
+--window_size 50 \
+--window_stride 5 \
 --n_epochs 100 \
 --start_lr 1e-3 \
 --min_lr 1e-5 \
---batch_size 16 \
+--batch_size 32 \
 --print_every_batch 50 \
---save_every_epoch 50 \
+--save_every_epoch 100 \
 --seed 100 \
 --cuda \
---flip_pitch \
+--bidirectional \
+--fpe \
+--fpi \
+--pe_z_dim 16 \
+--pi_z_dim 16 \
+--rnn_dim 256 \
+--hybrid_rnn_dim 128 \
+--n_layers 2 \
+--n_heads 4 \
+--dropout 0.0 \
+--cartesian_accel \
+--deriv_accum \
+--dynamic_hybrid \
