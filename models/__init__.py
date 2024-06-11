6fba88965c88e248370262c4ea36f82505d72f37
choi <95gkswns@naver.com>
2024-06-11 09:59:22 +0900

    ⚰️ Remove dead codes

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,6 @@ from models.graph_imputer.graph_imputer import BidirectionalGraphImputer
 from models.naomi.naomi import NAOMI
 from models.nrtsi.nrtsi import NRTSI
 
-# from .baselines.social_lstm.social_lstm import SOCIALLSTM
-
-
 def load_model(model_name, params, parser=None):
     model_name = model_name.lower()
 
